68b745fbd72390da2b42b4dfcd73be508593a502
gilson1506 <diurogilson@gmail.com>
2025-09-15 18:27:16 +0100

    Adapta backend para Vercel

--- api/index.js
+++ api/index.js
@@ -1,5 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({
-    message: '✅ API de integração com Pagar.me rodando!'
-  });
+  res.status(200).send('✅ API de integração com Pagar.me rodando!');
 }
