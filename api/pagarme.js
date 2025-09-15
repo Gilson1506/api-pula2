9e5350f5ca162ec3f3791c85f878298724264923
gilson1506 <diurogilson@gmail.com>
2025-09-15 18:01:50 +0100

    Update Vercel configuration and API structure

--- api/pagarme.js
+++ api/pagarme.js
@@ -1,5 +1,3 @@
-import PagarmeService from '../backend/services/pagarmeService.js';
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
